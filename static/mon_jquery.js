fc678165e9a265a9c67d2a0b80b2c7aefda2c73f
Rodolphe Basse <Rodo@Rodolphes-MacBook-Pro.local>
2013-12-25 22:30:12 +0000

    modifies user account

--- static/mon_jquery.js
+++ static/mon_jquery.js
@@ -29,5 +29,46 @@ $(document).ready(function()
 				$('#creer_compte').slideDown('slow');
 			})
 		});
+		
+	
+	
+	
+	//===== Mise en place de l'animation de la page MON COMPTE =====//
+	
+	$('#bouton_favoris_user').click(
+		function()
+		{
+			$('#bouton_favoris_user').css('background','rgba(200,14,31,1)');
+			$('#bouton_thread_user').css('background','rgba(200,14,31,.5)');
+			$('#bouton_settings').css('background','rgba(200,14,31,.5)');
+			$('#gestion_thread, #settings').slideUp('fast',function()
+			{
+				$('#favoris_user').slideDown('slow');
+			})
+		})
+	
+	$('#bouton_thread_user').click(
+		function()
+		{
+			$('#bouton_thread_user').css('background','rgba(200,14,31,1)');
+			$('#bouton_favoris_user').css('background','rgba(200,14,31,.5)');
+			$('#bouton_settings').css('background','rgba(200,14,31,.5)');
+			$('#favoris_user, #settings').slideUp('fast',function()
+			{
+				$('#gestion_thread').slideDown('slow');
+			})
+		})
+		
+	$('#bouton_settings').click(
+		function()
+		{
+			$('#bouton_settings').css('background','rgba(200,14,31,1)');
+			$('#bouton_thread_user').css('background','rgba(200,14,31,.5)');
+			$('#bouton_favoris_user').css('background','rgba(200,14,31,.5)');
+			$('#favoris_user, #gestion_thread').slideUp('fast',function()
+			{
+				$('#settings').slideDown('slow');
+			})
+		})
 })
 	
